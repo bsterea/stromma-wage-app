4a300da5c0430238e290e900cd4992d230f734b5
bsterea <bsterea@yahoo.com>
2026-03-25 19:47:43 +0100

    Add Payroll model with employee and salary fields

--- stromma_payroll/models.py
+++ stromma_payroll/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from stromma_employees.models import Employee
 
-# Create your models here.
+class Payroll(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.employee} - {self.salary}"
