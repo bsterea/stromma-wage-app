f5316b46551d87fcb33bae50ae1f3438067d27b9
bsterea <bsterea@yahoo.com>
2026-03-31 18:31:51 +0200

    Register Employee and Role models in admin

--- stromma_employees/admin.py
+++ stromma_employees/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-from .models import Payroll
+from .models import Employee, Role
 
-admin.site.register(Payroll)
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "cpr", "status", "is_active")
+    search_fields = ("first_name", "last_name", "cpr")
+    list_filter = ("status", "is_active")
+
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ("name",)
